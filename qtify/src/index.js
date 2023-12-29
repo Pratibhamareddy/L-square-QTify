7dcc5a722684b33e118a001fe75c37ee596ee967
Your Name <you@example.com>
2023-12-29 20:42:48 +0530

    section

--- qtify/src/index.js
+++ qtify/src/index.js
@@ -2,27 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { createBrowserRouter, RouterProvider} from "react-router-dom"
-// import Homepage from './Pages/Homepage/Homepage'
+import { createBrowserRouter, RouterProvider} from "react-router-dom"
+import Homepage from './Pages/Homepage/Homepage'
 
 
 
-// export const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App/>,
-//     children: [
-//       {
-//         path:"/",
-//         element:<Homepage />
-//       }
-//     ]
-//   }
-// ])
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path:"/",
+        element:<Homepage />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<RouterProvider router={router}/>);
-root.render(<App />);
+root.render(<RouterProvider router={router}/>);
+// root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
